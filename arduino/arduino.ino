77f871e4f2e120d2be1359530b665eaf7f60b538
Chris Morrow <morrowc@ops-netman.net>
2022-12-04 19:07:29 -0500

    Set the work on LEDs to happen each quantum.
    Define a quantun (DELAY), track time between loop start
    and loop end, only restart loop if the quantum has expired.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -23,12 +23,16 @@
 const char* URL = "http://mailserver.ops-netman.net:6789/status";
 const char* SSID = "theaternet";
 const char* PASS = "network123";
+// Delay betwen web requests and possible change to lights.
+const unsigned long DELAY = 100;
 // The delimiter between reply parts from the controller.
 const char* DELIMITER = ", ";
 // The current timestamp value from the previous controller reply.
 String CURRENT = "";
 
 CRGB leds[NUM_LEDS];
+unsigned long TOTAL_INTERVALS = 0;
+unsigned long WAIT_TIME = 0;
 
 
 void setup() {
@@ -59,6 +63,8 @@ void setup() {
 
 void loop()
 {
+  Serial.printf("Millis: %d\n", millis());
+  int st = millis();
   // Create an http client in this version of the loop, collect data from
   // remote server.
   WiFiClient client;
@@ -67,18 +73,21 @@ void loop()
   // Set the default dictate to 'rainbow'.
   String DICTATE = "rainbow";
 
-  Serial.println();
-  Serial.println("Starting http client request");
+  // Serial.println();
+  // Serial.println("Starting http client request");
   http.begin(client, url.c_str());
   int httpResponseCode = http.GET();
   if (httpResponseCode>0) {
-    Serial.printf("HTTP Response Code: %d\r\n", httpResponseCode);
+    // Serial.printf("HTTP Response Code: %d\r\n", httpResponseCode);
     String payload = http.getString();
-    Serial.println(payload);
+    // Serial.println(payload);
     // Convert payload from String to char[] and from char[] to char*.
     int n = payload.length();
     char p_array[n + 1];
     strcpy(p_array, payload.c_str());
+    TOTAL_INTERVALS++;
+    WAIT_TIME += (millis() - st);
+    Serial.printf("%d / %d == %d\n", WAIT_TIME, TOTAL_INTERVALS, WAIT_TIME / TOTAL_INTERVALS);
 
     // Use strtok() to tokenize the http payload.
     // First token should be the timestamp, second is the dictate.
@@ -91,42 +100,39 @@ void loop()
         token = strtok(NULL, DELIMITER);
         if ( token != NULL ) { DICTATE = token; }
         DICTATE.trim();
-        Serial.printf("\nTS: %s Dictate: %s\n", CURRENT.c_str(), DICTATE);
-        Serial.println();
+        // Serial.printf("\nTS: %s Dictate: %s\n", CURRENT.c_str(), DICTATE);
+        // Serial.println();
         // Display single color for now.
         // fill_rainbow(leds, NUM_LEDS, 0, 5);
         CRGB::HTMLColorCode color;
         if (strcmp(DICTATE.c_str(), "red") == 0 ) {
           color = CRGB::Red;
-          Serial.println("Reset color to red");
+          // Serial.println("Reset color to red");
         } else if (strcmp(DICTATE.c_str(), "orange") == 0 ) {
           color = CRGB::Orange;
-          Serial.println("Reset color to orange");
+          // Serial.println("Reset color to orange");
         } else if (strcmp(DICTATE.c_str(), "yellow") == 0) {
           color = CRGB::Yellow;
-          Serial.println("Reset color to yellow");
+          // Serial.println("Reset color to yellow");
         } else if (strcmp(DICTATE.c_str(), "green") == 0) {
           color = CRGB::Green;
-          Serial.println("Reset color to green");
+          // Serial.println("Reset color to green");
         } else if (strcmp(DICTATE.c_str(), "blue") == 0) {
           color = CRGB::Blue;
-          Serial.println("Reset color to blue");
+          // Serial.println("Reset color to blue");
         } else if (strcmp(DICTATE.c_str(), "indigo") == 0) {
           color = CRGB::Indigo;
-          Serial.println("Reset color to indigo");
+          // Serial.println("Reset color to indigo");
         } else if (strcmp(DICTATE.c_str(), "violet") == 0) {
           color = CRGB::Violet;
-          Serial.println("Reset color to violet");
+          // Serial.println("Reset color to violet");
         } else {
           color = CRGB::Red;
-          Serial.println("Reset color to default red");
+          // Serial.println("Reset color to default red");
         }
-        // blink along the light strip the new color.
-        for (int i = 0; i < NUM_LEDS ; i++ ) {
-          leds[i] = color;
-          FastLED.show();
-          delay(30);
-          leds[i] = CRGB::Black;
+        for (int i = 0; i < NUM_LEDS; i++) {
+            leds[i] = color;
+            FastLED.show();
         }
         Serial.println();
       }
@@ -134,9 +140,12 @@ void loop()
   } else {
     // 
     pride();
+    delay(1000);
     // FastLED.show();  
   }
-    delay(5000);
+  while ( (st + millis()) < DELAY ) {
+    // delay(500);
+  }
 }
 
 
